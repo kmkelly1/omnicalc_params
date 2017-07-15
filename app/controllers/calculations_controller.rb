6e7a100be5b8f32a4b8cc1e237b25264a62e2a87
Kate Kelly <kmkelly1@chicagobooth.edu>
2017-07-15 20:52:55 +0000

    First Save

---
+++ app/controllers/calculations_controller.rb
@@ -0,0 +1,93 @@
+class CalculationsController < ApplicationController
+    def flex_square
+        # the incoming parameters for this action look like {"a_number" => "5"}
+        # Rail's stores that has in a variable called params
+        
+        @user_number = params["a_number"].to_i
+        @squared_number = @user_number**2
+        
+        render("calculations/flexible_square_template.html.erb")
+    end
+    
+    def flex_square_root
+        # the incoming parameters for this action look like {"a_number" => "5"}
+        # Rail's stores that has in a variable called params
+        
+        @user_number = params["a_number"].to_i
+        @square_root_number = Math.sqrt(@user_number)
+        render ("calculations/flexible_square_root_template.html.erb")
+    end
+    def flex_payment
+        # the incoming parameters look like
+        # :a_number interest rate
+        # :b_number years
+        # :c_number amount
+        @interest_rate = params["a_number"].to_f / 100
+        @years = params["b_number"].to_f
+        @loan_amount = params["c_number"]
+        
+        @number_of_payments = @years * 12
+        @number_of_payments.to_f
+        @monthly_interest_rate = (@interest_rate) / 12
+        @one_plus_monthly = @monthly_interest_rate + 1
+        @monthly_number_of_payments = @one_plus_monthly**@number_of_payments
+        @numerator = @monthly_number_of_payments * @monthly_interest_rate
+        @divisor = @monthly_number_of_payments- 1
+        
+        @monthly_payment = @loan_amount * ( @numerator / @divisor)
+        
+        render ("calculations/flexible_payment_template.html.erb")
+    end
+    
+    def square_form
+        
+        render("calculations/square_form_template.html.erb")
+    end
+    
+    def process_square
+        
+        @user_number = params["the_user_number"].to_i
+        @squared_number = @user_number**2
+        
+        render("calculations/square_results_template.html.erb")
+    end
+    
+    def payment_form
+        
+       
+        render("calculations/payment_form_template.html.erb")
+    end
+    
+    def process_payment
+        
+        @interest_rate = params["the_user_interest_rate"].to_f
+        @years = params["the_user_years"].to_f
+        @principal = params["the_user_principal"].to_f
+        
+        
+        @number_of_payments = @years * 12
+        @number_of_payments.to_f
+        @monthly_interest_rate = (@interest_rate / 100) / 12
+        @one_plus_monthly = @monthly_interest_rate + 1
+        @monthly_number_of_payments = @one_plus_monthly**@number_of_payments
+        @numerator = @monthly_number_of_payments * @monthly_interest_rate
+        @divisor = @monthly_number_of_payments- 1
+        
+        @monthly_payment_2 = @principal * ( @numerator / @divisor)
+        
+        render("calculations/payment_results_template.html.erb")
+    end
+    
+    def random_form
+        render ("calculations/random_form_template.html.erb")
+    end
+    
+    def random_number_results
+        @user_number_2 = params["user_number_2"].to_f
+        @user_number_1 = params["user_number_1"].to_f
+        @random_placeholder = @user_number_2 - @user_number_1 + 1
+        @random_number = rand(@random_placeholder) + @user_number_1 
+        render("calculations/random_number_results_template.html.erb")
+    end
+        
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get("/", {:controller => "calculations", :action => "flex_square"})
+  get("/flexible/square/:a_number", { :controller => "calculations", :action =>"flex_square" })
+  get("/flexible/square_root/:a_number", { :controller => "calculations", :action => "flex_square_root"})
+  get("/flexible/payment/:a_number/:b_number/:c_number", { :controller => "calculations", :action =>"flex_payment"})
+  
+  get("/square/new", { :controller => "calculations", :action => "square_form"})
+  get("/square/results", { :controller => "calculations", :action => "process_square"})
+  
+  get("/payment/new", { :controller => "calculations", :action => "payment_form"})
+  get("/payment/results", { :controller => "calculations", :action => "process_payment"})
+  
+  get("/random/new", { :controller => "calculations", :action => "random_form"})
+  get("/random/results", { :controller => "calculations", :action => "random_number_results"})
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount WebGit::Engine, at: "/rails/git"
 end
